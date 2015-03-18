476e70bb830220d703c0f40b088ff2110fcfefb0
Crystal Contreras-Grossman and Gabe Finch <gabefinch@gmail.com>
2015-03-17 17:17:30 -0700

    Add sections/most section pages, lessons within sections views mods still needed

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,8 +1,13 @@
 class Lesson < ActiveRecord::Base
+  belongs_to :section
   validates :name, :presence => true
   validates :text, :presence => true
 
   def next
     Lesson.find_by number: number + 1
+  end
+
+  def prev
+    Lesson.find_by number: number - 1
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root :to => 'lessons#index'
-
-  resources :lessons
+  root :to => 'sections#index'
+  resources :sections do
+    resources :lessons
+  end
 end
